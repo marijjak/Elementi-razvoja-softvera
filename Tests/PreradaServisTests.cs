5e50509d7694ca02a68e5921040778337ea231bb
marijjak <marijakrsmanovicbn@gmail.com>
2026-02-05 20:21:51 +0100

    tests fixed

--- Tests/PreradaServisTests.cs
+++ Tests/PreradaServisTests.cs
@@ -9,62 +9,74 @@ using Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tests
 {
     public class PreradaServisTests
     {
-        private Mock<IPerfumeRepository> _parfemRepoMock = null;
-        private Mock<IDogadjajiServis> _dogadjajiMock = null;
+        private Mock<IPerfumeRepository> _parfemRepoMock = null!;
         private Mock<IBiljkeServis> _biljkeServisMock = null!;
-        private Mock<IBiljkeRepozitorijum> _biljkeRepoMock = null;
-        private PreradaServis _servis = null;
-
-
+        private Mock<IBiljkeRepozitorijum> _biljkeRepoMock = null!;
+        private PreradaServis _servis = null!;
 
         [SetUp]
         public void Setup()
         {
-            // Kreiramo "lažne" verzije zavisnosti
             _parfemRepoMock = new Mock<IPerfumeRepository>();
-            _dogadjajiMock = new Mock<IDogadjajiServis>();
             _biljkeServisMock = new Mock<IBiljkeServis>();
             _biljkeRepoMock = new Mock<IBiljkeRepozitorijum>();
-            // Inicijalizujemo servis sa tim lažnim podacima
 
             _servis = new PreradaServis(
-            _biljkeServisMock.Object,
-            _parfemRepoMock.Object,
-            _biljkeRepoMock.Object
+                _biljkeServisMock.Object,
+                _parfemRepoMock.Object,
+                _biljkeRepoMock.Object
             );
         }
 
+        [Test]
         public void NapraviParfem_ValidniPodaci_IspravnaKalkulacijaKolicine()
         {
-            // Arrange - Postavljanje ulaznih podataka
             string naziv = "TestParfem";
             int brojBocica = 10;
-            int zapremina = 150; 
+            int zapremina = 150;
             string tip = "Eau de Parfum";
 
-            _biljkeServisMock.Setup(s => s.SveBiljke()).Returns(new List<Biljka> {
-    new Biljka { Stanje = StanjeBiljke.Ubrana, JacinaArome = 3.0 }
-});
+        
+            var biljke = new List<Biljka>();
+            for (int i = 1; i <= 200; i++)
+            {
+                biljke.Add(new Biljka
+                {
+                    Id = Guid.NewGuid(),
+                    Stanje = StanjeBiljke.Ubrana,
+                    JacinaArome = 3.0
+                });
+            }
 
-            // Act - Izvršavanje metode koju testiramo
-            var rezultat = _servis.NapraviParfem(naziv, brojBocica, zapremina, tip);
+            _biljkeServisMock.Setup(s => s.SveBiljke()).Returns(biljke);
 
-            // Assert - Provera rezultata
-            ClassicAssert.That(rezultat, Is.Not.Null);
-            ClassicAssert.That(rezultat.Naziv, Is.EqualTo(naziv));
+           
+            _biljkeServisMock.Setup(s => s.DodajBiljku(It.IsAny<Biljka>())).Returns(true);
 
-            // KLJUČNA PROVERA: Da li je mililitraža ispravno izračunata (broj * zapremina)
-            ClassicAssert.AreEqual(brojBocica, rezultat.KolicinaNaStanju);
-            ClassicAssert.AreEqual(zapremina, rezultat.ZapreminaBociceMl);
+           
+            _parfemRepoMock.Setup(r => r.Dodaj(It.IsAny<Parfem>()))
+                          .Returns((Parfem p) => p);
 
-            // Provera da li je repozitorijum pozvan da sačuva parfem
+            Parfem parfem;
+            bool rezultat = _servis.NapraviParfem(naziv, brojBocica, zapremina, tip, out parfem);
+
+            ClassicAssert.IsTrue(rezultat);
+            ClassicAssert.IsNotNull(parfem);
+
+            ClassicAssert.AreEqual(naziv, parfem.Naziv);
+            ClassicAssert.AreEqual(tip, parfem.TipParfema);
+
+            ClassicAssert.AreEqual(brojBocica, parfem.KolicinaNaStanju);
+            ClassicAssert.AreEqual(zapremina, parfem.ZapreminaBociceMl);
+
+       
+            ClassicAssert.AreEqual(brojBocica * zapremina, parfem.UkupnaKolicinaMl);
+
             _parfemRepoMock.Verify(r => r.Dodaj(It.IsAny<Parfem>()), Times.Once);
         }
     }
--- Tests/Services/BiljkeServisTests.cs
+++ Tests/Services/BiljkeServisTests.cs
@@ -1,23 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain.Enumeracije;
+﻿using Domain.Enumeracije;
+using Domain.Modeli;
 using Domain.Repozitorijumi;
 using Domain.Servisi;
 using Moq;
 using NUnit.Framework;
 using Services;
+using System;
 
 namespace Tests.Services
 {
     [TestFixture]
     public class BiljkeServisTests
     {
-        private Mock<IBiljkeRepozitorijum> _mockRepo = null;
-        private Mock<IDogadjajiServis> _mockLogger = null;
-        private BiljkeServis _servis = null;
+        private Mock<IBiljkeRepozitorijum> _mockRepo = null!;
+        private Mock<IDogadjajiServis> _mockLogger = null!;
+        private BiljkeServis _servis = null!;
 
         [SetUp]
         public void Setup()
@@ -30,19 +27,26 @@ namespace Tests.Services
         [Test]
         public void ZasadiNovuBiljku_Uspesno_PozivaLogger()
         {
+          
+            _mockRepo
+                .Setup(r => r.Dodaj(It.IsAny<Biljka>()))
+                .Returns((Biljka b) => b); 
 
-            _mockLogger.Setup(l => l.Zabelezi(It.IsAny<string>(), It.IsAny<TipEvidencije>(), It.IsAny<Guid?>()))
-    .Returns(true);
+            _mockLogger
+                .Setup(l => l.Zabelezi(It.IsAny<string>(), It.IsAny<TipEvidencije>(), It.IsAny<Guid?>()))
+                .Returns(true);
+
             var rezultat = _servis.ZasadiNovuBiljku("Kamilica", "Matricaria chamomilla", "Srbija", 4.0);
 
-
             Assert.That(rezultat, Is.True);
-            _mockLogger.Verify(l => l.Zabelezi(
-                It.Is<string>(s => s.Contains("Zasađena nova biljka")),
-                TipEvidencije.INFO,
-                It.IsAny<Guid?>()),
-            Times.Once);
-        }
 
+            _mockRepo.Verify(r => r.Dodaj(It.IsAny<Biljka>()), Times.Once);
+
+            _mockLogger.Verify(l => l.Zabelezi(
+                    It.Is<string>(s => s.Contains("Zasađena nova biljka")),
+                    TipEvidencije.INFO,
+                    It.IsAny<Guid?>()),
+                Times.Once);
+        }
     }
 }
